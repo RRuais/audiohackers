d0e199158a3590449aaf107ac0f4abe0193f8efd
Rich Ruais <richruais@gmail.com>
2017-03-27 22:02:22 -0700

    changed config vars

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -1,9 +1,4 @@
 module.exports = {
   dbURI: process.env.dbURI,
   sessionSecret: process.env.sessionSecret,
-  // facebook: {
-  //   clientID: '1292626007485364',
-  //   clientSecret: 'ffd434980b35f57a17dae317e93351a3',
-  //   callbackURL: 'http://localhost:8000/api/users/facebook/callback',
-  // }
 };
